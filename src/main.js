470ac2d3f9528662ccd89e54bda332fd66415753
b9.dead <bj.dead@gmail.com>
2019-10-26 15:21:05 +0530

    admin authentication

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import  jQuery from 'jquery'
+import jQuery from 'jquery'
 import 'popper.js'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/app.scss'
+import {fb} from "./firebase.js"
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
@@ -14,7 +15,14 @@ window.$ = window.jQuery = jQuery
 
 Vue.component('Navbar', require('./components/Navbar.vue').default)
 
-new Vue({
-	router,
-	render: h => h(App)
-}).$mount("#app");
+let app = "";
+
+fb.auth().onAuthStateChanged(function (user) {
+	if (!app) {
+		new Vue({
+			router,
+			render: h => h(App)
+		}).$mount("#app");
+
+	}
+});
