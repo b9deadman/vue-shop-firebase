4e98f30cdf581090eb1019bc71b35c27bb242f29
b9.dead <bj.dead@gmail.com>
2019-10-28 17:52:53 +0530

    reset email to user

--- src/views/admin/Profile.vue
+++ src/views/admin/Profile.vue
@@ -1,145 +1,151 @@
 <template>
     <b-container fluid>
-        <b-row>
-            <b-col>
-                <h3>Profile settings</h3>
-                <p>Change your profile settings here</p>
-            </b-col>
-            <b-col>
-                <img src="/img/svg/profile.svg" width="300" alt class="img-fluid" />
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-container fluid>
-                <b-col cols="8">
-                    <b-card no-body>
-                        <b-tabs pills card vertical>
-                            <b-tab title="Profile" active>
-                                <b-card-text>
-                                    <b-form-group>
-                                        <b-form-input
-                                            v-model="profile.name"
-                                            placeholder="Enter your name"
-                                            class="mt-2"
-
-                                        ></b-form-input>
-                                    </b-form-group>
-                                    <b-form-group>
-                                        <b-form-input
-                                            v-model="profile.phone"
-                                            placeholder="Enter your phone"
-                                            class="mt-2"
-                                        ></b-form-input>
-                                    </b-form-group>
-                                    <b-form-group>
-                                        <b-form-input
-                                            v-model="profile.address"
-                                            placeholder="Enter your address"
-                                            class="mt-2"
-                                        ></b-form-input>
-                                    </b-form-group>
-                                    <b-form-group>
-                                        <b-form-input
-                                            v-model="profile.landmark"
-                                            placeholder="Enter any land mark"
-                                            class="mt-2"
-                                        ></b-form-input>
-                                    </b-form-group>
-                                    <b-from-group>
-                                        <b-button
-                                            variant="success"
-                                            class="float-right mt-2 mb-2"
-                                            @click="updateProfile"
-                                        >Save</b-button>
-                                    </b-from-group>
-                                </b-card-text>
-                            </b-tab>
-                            <b-tab title="Account">
-                                <b-card-text>
-                                    <b-card-text>
-                                        <b-form-group>
-                                            <b-form-input
-                                                v-model="account.user"
-                                                placeholder="User Name"
-                                                class="mt-2"
-                                            ></b-form-input>
-                                        </b-form-group>
-                                        <b-form-group>
-                                            <b-form-input
-                                                v-model="account.email"
-                                                placeholder="Email Address"
-                                                class="mt-2"
-                                            ></b-form-input>
-                                        </b-form-group>
-                                        <b-form-group>
-                                            <b-form-input
-                                                v-model="account.password1"
-                                                placeholder="New Password"
-                                                class="mt-2"
-                                            ></b-form-input>
-                                        </b-form-group>
-                                        <b-form-group>
-                                            <b-form-input
-                                                v-model="account.password2"
-                                                placeholder="Confirm Password"
-                                                class="mt-2"
-                                            ></b-form-input>
-                                        </b-form-group>
-                                        <b-form-group>
-                                            <b-form-file></b-form-file>
-                                        </b-form-group>
-                                        <b-from-group>
-                                            <b-button
-                                                variant="success"
-                                                class="float-right mt-2 mb-2"
-                                                @click="updateAccount"
-                                            >Save</b-button>
-                                        </b-from-group>
-                                    </b-card-text>
-                                </b-card-text>
-                            </b-tab>
-                        </b-tabs>
-                    </b-card>
-                </b-col>
-            </b-container>
-        </b-row>
+        <h3>Profile settings</h3>
+        <br />
+        <p>Change your profile settings here</p>
+        <hr />
+        <b-card no-body>
+            <b-tabs pills card>
+                <b-tab title="Profile" active>
+                    <b-card-text>
+                        <b-form-group>
+                            <b-form-input
+                                v-model="profile.name"
+                                placeholder="Enter your name"
+                                class="mt-2"
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-form-group>
+                            <b-form-input
+                                v-model="profile.phone"
+                                placeholder="Enter your phone"
+                                class="mt-2"
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-form-group>
+                            <b-form-input
+                                v-model="profile.address"
+                                placeholder="Enter your address"
+                                class="mt-2"
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-form-group>
+                            <b-form-input
+                                v-model="profile.landmark"
+                                placeholder="Enter any land mark"
+                                class="mt-2"
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-from-group>
+                            <b-button
+                                variant="success"
+                                class="float-right mt-2 mb-2"
+                                @click="updateProfile"
+                            >Save</b-button>
+                        </b-from-group>
+                    </b-card-text>
+                </b-tab>
+                <b-tab title="Account">
+                    <b-card-text>
+                        <b-card-text>
+                            <b-form-group>
+                                <b-form-input
+                                    v-model="account.user"
+                                    placeholder="User Name"
+                                    class="mt-2"
+                                ></b-form-input>
+                            </b-form-group>
+                            <b-form-group>
+                                <b-form-input
+                                    v-model="account.email"
+                                    placeholder="Email Address"
+                                    class="mt-2"
+                                ></b-form-input>
+                            </b-form-group>
+                            <b-form-group>
+                                <b-form-input
+                                    v-model="account.password1"
+                                    placeholder="New Password"
+                                    class="mt-2"
+                                ></b-form-input>
+                            </b-form-group>
+                            <b-form-group>
+                                <b-form-input
+                                    v-model="account.password2"
+                                    placeholder="Confirm Password"
+                                    class="mt-2"
+                                ></b-form-input>
+                            </b-form-group>
+                            <b-form-group>
+                                <b-form-file></b-form-file>
+                            </b-form-group>
+                            <b-from-group>
+                                <b-button
+                                    variant="success"
+                                    class="float-right mt-2 mb-2 ml-2"
+                                    @click="updateAccount"
+                                >Save</b-button>
+                                <b-button
+                                    variant="success"
+                                    class="float-right mt-2 mb-2 ml-2"
+                                    @click="resetPassword"
+                                >Reset Password</b-button>
+                            </b-from-group>
+                        </b-card-text>
+                    </b-card-text>
+                </b-tab>
+            </b-tabs>
+        </b-card>
     </b-container>
 </template>
 <script>
-import {fb, db} from '@/firebase.js'
+import { fb, db } from "@/firebase.js";
 
 export default {
     name: "profile",
-    data(){
-        return{
-            profile:{
-                name:null,
-                phone:null,
-                address:null,
-                landmark:null,
+    data() {
+        return {
+            profile: {
+                name: null,
+                phone: null,
+                address: null,
+                landmark: null
             },
-            account:{
-                name:null,
-                email:null,
-                photoUrl:null,
-                emailVerified:null,
-                password:null,
-                confirmPassword:null,
-                uid:null,
+            account: {
+                name: null,
+                email: null,
+                photoUrl: null,
+                emailVerified: null,
+                password: null,
+                confirmPassword: null,
+                uid: null
             }
-        }
+        };
     },
-    firestore(){
+    firestore() {
         const user = fb.auth().currentUser;
-        return{
-            profile: db.collection('profiles').doc(user.uid)
-        }
+        return {
+            profile: db.collection("profiles").doc(user.uid)
+        };
     },
-    methods:{
-        updateProfile(){
-            this.$firestore.profile.update(this.profile)
+    methods: {
+        resetPassword() {
+            const auth = fb.auth();
+            auth.sendPasswordResetEmail(auth.currentUser.email)
+                .then(() => {
+                    Toast.fire({
+                        type: 'success',
+                        title: 'Email Sent'
+                    })
+                })
+                .catch((error) => {
+                    alert(error)
+                });
         },
-        uploadImage(){},
+        updateProfile() {
+            this.$firestore.profile.update(this.profile);
+        },
+        uploadImage() {}
     }
 };
 </script>
